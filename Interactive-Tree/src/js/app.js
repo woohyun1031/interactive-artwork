da26bb25dd9ac8bcb0222096a11f260a12bb8b07
KimWooHyun <ktkwhms3@gmail.com>
2023-05-31 14:38:30 +0900

    Feat: Tree 뼈대 생성

---
+++ Interactive-Tree/src/js/app.js
@@ -0,0 +1,30 @@
+import { Tree } from "./tree.js";
+
+class App {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    document.body.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext("2d");
+    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+    window.addEventListener("resize", this.resize.bind(this), false);
+    this.resize();
+    new Tree(this.ctx, this.stageWidth / 2, this.stageHeight);
+  }
+  resize() {
+    // body의 너비와 높이 저장
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    // 디스플레이 비율에 맞추어 캔버스 사이즈와 비율 조정
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+    this.canvas.height = this.stageHeight * this.pixelRatio;
+    this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+    // 리사이즈시 캔버스를 비워줌
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+  }
+}
+
+window.onload = () => {
+  new App();
+};
---
+++ Interactive-Tree/src/js/tree.js
@@ -0,0 +1,43 @@
+import { Branch } from "./branch.js";
+
+export class Tree {
+  constructor(ctx, posX, posY) {
+    this.ctx = ctx;
+    this.posX = posX;
+    this.posY = posY;
+    this.branches = [];
+    this.depth = 5;
+
+    this.init();
+  }
+  init() {
+    this.createBranch(this.posX, this.posY, -90, 0);
+    this.draw(this.ctx);
+  }
+  createBranch(startX, startY, angle, depth) {
+    if (depth === this.depth) return;
+
+    // 가지 생성
+    const len = 100;
+    const endX = startX + this.cos(angle) * len;
+    const endY = startY + this.sin(angle) * len;
+    this.branches.push(new Branch(startX, startY, endX, endY));
+    this.createBranch(endX, endY, angle - 30, depth + 1);
+    this.createBranch(endX, endY, angle + 30, depth + 1);
+  }
+  draw(ctx) {
+    // 가지들을 캔버스에 draw
+    for (let i = 0; i < this.branches.length; i++) {
+      this.branches[i].draw(ctx);
+    }
+  }
+  cos(angle) {
+    return Math.cos(this.degToRad(angle));
+  }
+  sin(angle) {
+    return Math.sin(this.degToRad(angle));
+  }
+  degToRad(angle) {
+    return (angle / 180.0) * Math.PI;
+  }
+}
