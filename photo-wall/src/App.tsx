40dd66d379a12beda2cac71a14246a630de22cd4
김우현 <ktkwhms3@gmail.com>
2025-04-22 18:22:08 +0900

    Feat: implement basic draggable box component

---
+++ photo-wall/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import BasicDragBox from "src/BasicDragBox.tsx";
+
+function App() {
+
+    return (
+        <div>
+            <h1 style={{ textAlign: 'center' }}>📸 Photo Wall</h1>
+            <BasicDragBox />
+        </div>
+    )
+}
+
+export default App
